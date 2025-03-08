da832ad5efc3c32070f77077bf81e3c97c77940d
mamu2119 <chaitanyapandya1985@gmail.com>
2025-03-08 15:26:06 +0530

    Add files via upload

---
+++ Percentage Calculation.py
@@ -0,0 +1,9 @@
+Maths = 69
+English = 62
+Science = 60
+Gujarati = 61
+sum = Maths+English+Science+Gujarati
+print("Sum of the marks of all 4 subjects are: ", sum)
+perc = (sum/320)*100
+print(end="Percentage Mark = ")
+print(perc)
